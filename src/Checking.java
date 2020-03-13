ac31c12167e24a15d744c41ee694d897acaffd71
dohpkim <dohp.kim@gmail.com>
2020-03-13 16:15:23 +0900

    200313

--- src/Checking.java
+++ src/Checking.java
@@ -5,28 +5,32 @@ import java.util.stream.IntStream;
 public class Checking {
 	private static int checksum;
 	
-	public static <T extends List<String>> int checkNynum(T list) {
+	public Checking() {
+		checksum = 0;
+	}
+	
+	private <T extends List<String>> void checkMynum(T list) {
 		try {
 			list.stream()
 				.peek(s-> {
 					if(s.length() == 0) {
 						checksum = -2;
-						return;
 					}
 				})
 				.mapToInt(s-> Integer.valueOf(s))
+				
 				.forEach(n-> System.out.print(n + "\t"));
 			
 		} catch (NumberFormatException e) {
-			return checksum = -1;
-		}	
-			
-					
+			if(checksum == 0)
+				checksum = -1;
+		}
 		
-		
-		
-		
-		
+	}
+	
+	public <T extends List<String>> int getChecksum(T list) {
+		checkMynum(list);
 		return checksum;
+		
 	}
 }
